6e84e19f970ba4b5083549079d828c68cfe1bdaa
jits98 <jits8809@gmail.com>
2026-01-31 07:58:22 +0530

    fibonacci project set-up

---
+++ fibonacci-number/target/debug/deps/fibonacci_number-2cb373a455de967e.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/rust-exercises/fibonacci-number/target/debug/deps/fibonacci_number-2cb373a455de967e.d: src/main.rs
+
+/Users/home/Desktop/rust-exercises/fibonacci-number/target/debug/deps/libfibonacci_number-2cb373a455de967e.rmeta: src/main.rs
+
+src/main.rs:
---
+++ fibonacci-number/target/debug/deps/fibonacci_number-a758c32e1a943a6b.d
@@ -0,0 +1,5 @@
+/Users/home/Desktop/rust-exercises/fibonacci-number/target/debug/deps/fibonacci_number-a758c32e1a943a6b.d: src/main.rs
+
+/Users/home/Desktop/rust-exercises/fibonacci-number/target/debug/deps/libfibonacci_number-a758c32e1a943a6b.rmeta: src/main.rs
+
+src/main.rs:
