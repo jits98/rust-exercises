a9320389e2e6280d87cf46ed6fd84c012b52a827
jits98 <jits8809@gmail.com>
2026-02-01 10:11:56 +0530

    add logic for fibonacci number generator

--- fibonacci-number/src/main.rs
+++ fibonacci-number/src/main.rs
@@ -4,45 +4,40 @@ use std::io;
 fn main() {
     println!("This is fibonacci number generator project.");
     
-    println!("Please enter the position number which you want you want to get the fibonacci number:");
+    println!("Please enter the position you want in Fibonacci series:");
 
     // Asking user for the position for which he/she want the fibonacci number
-
-    let num_position: u32;
+    let num_position: usize;
     
     loop {      
         // taking fibonacci series second number from the user
-        let mut input3 = String::new();
+        let mut input = String::new();
         // reading the user input 
         io::stdin()
-            .read_line(&mut input3)
-            .expect("Failed to read number.");      
-        match input3.trim().parse::<u32> () {
-            Ok(num3) => {
-                println!("You entered: {} as position number for the fibonacci number", num3);
-                num_position = num3;
+            .read_line(&mut input)
+            .expect("Failed to read input.");  
+
+        match input.trim().parse::<usize>() {
+            Ok(n) if n > 0 => {   
+                num_position = n;
                 break;
             }
-            Err(_) => {
-                println!("Please enter a valid number: ");
+            _ => {
+                println!("Please enter a valid positive number: ");
             }
         }         
     };
 
-    const FIRST_NUMBER: u32 = 0;
-        const SECOND_NUMBER: u32 = 1;
-       
-        let mut vec = Vec::new();
-
-        vec.push(FIRST_NUMBER);
-        vec.push(SECOND_NUMBER);
-        vec.push(FIRST_NUMBER + SECOND_NUMBER);
-
-        vec.push(SECOND_NUMBER + vec.get(2));
-        println!("{:?}", vec);
-        println!("{}", num_position);
-
     // Fibonacci number Output needs to change the logic here
+
+    if num_position == 1 {
+        println!("Fibonacci number at position 1 is 0");
+        return;
+    } 
+    if num_position == 2 {
+        println!("Fibonacci number at position 2 is 1");
+        return;
+    }
    
     
 
