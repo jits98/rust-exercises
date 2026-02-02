9909ee89756cbddc3fadd00deac6359ff8414a98
jits98 <jits8809@gmail.com>
2026-02-02 09:47:14 +0530

    add logic for printing lyrics

--- twelve-days-print-lyrics/src/main.rs
+++ twelve-days-print-lyrics/src/main.rs
@@ -27,3 +27,29 @@ fn print_day_intro(day: u32) {
         "On the {} day of Christmas my true love sent to me:", day_name
     );
 }
+
+fn print_gifts(day: u32) {
+    for gifts in (1..=day).rev() {
+        match gift {
+            12 => println!("Twelve drummers drumming,"),
+            11 => println!("Eleven pipers piping,"),
+            10 => println!("Ten lords a-leaping,"),
+            9 => println!("Nine ladies dancing,"),
+            8 => println!("Eight maids a-milking,"),
+            7 => println!("Seven swans a-swimming,"),
+            6 => println!("Six geese a-laying,"),
+            5 => println!("Five gold rings,"),
+            4 => println!("Four calling birds,"),
+            3 => println!("Three french hens,"),
+            2 => println!("Two turtle doves,"),
+            1 => {
+                if day == 1 {
+                    println!("A partridge in a peer tree.");
+                } else {
+                    println!("And a partridge in a pear tree.")
+                }
+            }
+            _ => {}
+        }
+    }
+}
