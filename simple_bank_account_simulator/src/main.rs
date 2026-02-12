9a82cb7f0186768c5ef2d76e71cb6f27c80a7c29
jits98 <jits8809@gmail.com>
2026-02-12 09:05:15 +0530

    completed project

--- simple_bank_account_simulator/src/main.rs
+++ simple_bank_account_simulator/src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 fn main() {
-    initial_balance();
     choose_an_action();
 }
 
@@ -22,22 +21,55 @@ fn initial_balance() -> u32 {
     }
 }
 
-fn choose_an_action() -> u32 {
+fn choose_an_action() {
+    let balance = initial_balance();
     println!("Choose an action:");
     println!("1. Deposit\n2. Withdraw\n3. Check Balance");
+    
+    loop {
+        let mut input = String::new();
+        
+        io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read the input");
+    
+    let amount = math_on_amount();
+    
+         match input.trim().parse() {
+                Ok(1) => {
+                    let sum =  balance + amount;
+                    println!("Deposit Successful\nCurrent balance: {}", sum);
+                    break;
+                },
+                Ok(2) => {
+                    let sum =  balance - amount;
+                    println!("Withdrawal Successful\nCurrent balance: {}", sum);
+                    break;
+                },
+                Ok(3) => {
+                    println!("Current Balance: {}", balance);
+                    break;
+                },
+                _ => println!("Please enter correct option"),
+                
+        }
+    }
+}
+
+fn math_on_amount() -> u32 {
+    println!("Enter amount");
 
     loop {
         let mut input = String::new();
-
+    
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read the input");
 
         match input.trim().parse() {
-                1 => println!("Deposit successful"),
-                2 => println!("Withdrawal successful"),
-                3 => println!("Balance check"),
-                _ => println!("Please enter correct option"),
+            Ok(num) => return num,
+            Err(_) => println!("Please enter a valid number"),
         }
     }
+
 }
