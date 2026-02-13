bf0709fd1d1491631a3fc674570462ce21ba3075
jits98 <jits8809@gmail.com>
2026-02-13 10:18:57 +0530

    refactor code

--- simple-contact-book/src/main.rs
+++ simple-contact-book/src/main.rs
@@ -1,7 +1,14 @@
-use std::io;
+use std::{any::type_name, io};
 
+struct Person {
+    name: String,
+    number: u32,
+}
 fn main() {
     choose_an_option();
+    let mut people: Vec<Person> = Vec::new();
+    add_contact(&mut people);
+    println!("Total contacts: {}", people.len());
 }
 
 fn choose_an_option() {
@@ -16,10 +23,7 @@ fn choose_an_option() {
     
     
         match input.trim().parse() {
-            Ok(1) => {
-                add_contact();
-                break;
-            }
+            Ok(1) => println!("1"),
             Ok(2) => println!("2"),
             Ok(3) => println!("3"),
             _ => println!("Please enter a correct option number"),
@@ -27,7 +31,7 @@ fn choose_an_option() {
     }   
 }
 
-fn add_contact() {
+fn add_contact(people: &mut Vec<Person>) {
     println!("Enter name:");
 
     let mut name = String::new();
@@ -36,12 +40,30 @@ fn add_contact() {
         .read_line(&mut name)
         .expect("Failed to read the input");
 
+    let name = name.trim().to_string();
     println!("Enter phone:");
 
-    let mut number = String::new();
+    let number: u32 = loop {
+        let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Failed to read the input");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read the input");
+
+        match input.trim().parse() {
+        Ok(num) => break num,
+        Err(_) => {
+            println!("Please enter correct number");
+            continue;
+    }
+        }
+    };
+
+    people.push(Person {name, number});
+
     println!("Contact added!");
 }
+
+// fn view_contact() {
+
+// }
